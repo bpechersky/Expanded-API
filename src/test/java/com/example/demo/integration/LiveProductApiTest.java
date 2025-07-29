7bbb1d0b8680d06074f3e5396a3d2ddc4c40e7bf
bpechersky <bpechersky@gmx.com>
2025-07-29 16:21:09 -0700

    updated test createAndGetProductById

--- src/test/java/com/example/demo/integration/LiveProductApiTest.java
+++ src/test/java/com/example/demo/integration/LiveProductApiTest.java
@@ -2,6 +2,7 @@
 package com.example.demo.integration;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,17 +11,32 @@ import static org.hamcrest.Matchers.equalTo;
 public class LiveProductApiTest {
 
     @Test
-    public void createProductAgainstRunningApp() {
+    public void createAndGetProductById() {
         RestAssured.baseURI = "http://localhost:8080";
 
-        given()
+        // Create a new product
+        Response postResponse = given()
                 .contentType("application/json")
-                .body("{\"name\": \"LiveTestProduct\", \"price\": 42.42}")
+                .body("{\"name\": \"TestProductWithId\", \"price\": 55.55}")
                 .when()
                 .post("/api/products")
                 .then()
                 .statusCode(200)
-                .body("name", equalTo("LiveTestProduct"))
-                .body("price", equalTo(42.42f));
+                .body("name", equalTo("TestProductWithId"))
+                .body("price", equalTo(55.55f))
+                .extract().response();
+
+        int productId = postResponse.jsonPath().getInt("id");
+
+        // Get the product by ID
+        given()
+                .contentType("application/json")
+                .when()
+                .get("/api/products/" + productId)
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(productId))
+                .body("name", equalTo("TestProductWithId"))
+                .body("price", equalTo(55.55f));
     }
 }
